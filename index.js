f088aa7d12d18c34d0a3611650ce379f89072a03
cenrich <ceci_enrich@hotmail.com>
2019-08-15 21:38:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// Tarea: Crear un módulo en Node. El módulo debe usar una dependencia external. El módulo debe ser llamado desde el index
+// y darnos una respuesta.
+// Pasos:
+// - npm init 
+// - ignorar node_modules
+// - subirlo a un repo
